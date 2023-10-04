9027a24ee6f48b2b1a8a439f0e9028afde3a90dc
Niji <nijikokun@gmail.com>
2023-10-04 13:09:24 -0700

    feat: improve redirect system, add events and agent

--- static/scripts/fix-redirect.js
+++ static/scripts/fix-redirect.js
@@ -1,35 +1,83 @@
-const path = window.location.pathname
-const goto = (path) => {
-    window.location.href = `${window.location.origin}${path}`
+const fromExact = (from) => (path) => [ from, path === from ]               // [xyz]
+const fromIncludes = (from) => (path) => [ from, path.includes(from) ]      // [xy]z
+const toExact = (to) => () => to                                            // x -> y
+const toReplace = (to) => (path, from) => path.replace(from, to)            // abc/x -> xyz/x
+
+// List of redirects
+//  String values are treated as exacts by default.
+//  Exact matches should be listed first as redirects are stacked
+// 
+// To change behavior you can use the following method structures:
+//  [0 - from]: (path) => [matchStr, boolean (true for match, false for do not match)]
+//  [1 - to]:  (path, from) => string (returned value becomes the new path)
+const redirects = [
+    [ fromIncludes(`/docs/ngrok-link`), `/docs/cloud-edge` ],
+    [ fromIncludes(`/docs/api/api-clients`), `/docs/api/#client-libraries` ],
+    [ fromIncludes(`/docs/api/client-libraries`), `/docs/api/#client-libraries` ],
+    [ fromIncludes(`/docs/api/terraform`), `/docs/api/#terraform-provider` ],
+    [ fromIncludes(`/docs/platform/api`), `/docs/api/` ],
+    [ fromIncludes(`/docs/platform/events`), `/docs/events/` ],
+    [ fromIncludes(`/docs/events/filtering`), `/docs/events/#filters` ],
+    [ fromIncludes(`/docs/http-header-templates/`), `/docs/cloud-edge/http-header-templates/` ],
+    [ fromIncludes(`/docs/platform/pops/`), `/docs/cloud-edge/pops/` ],
+    [ fromIncludes(`/docs/best-practices/security-dev-productivity/`), `/docs/guides/security-dev-productivity/` ],
+    [ fromIncludes(`/docs/platform/ip-policies/`), `/docs/cloud-edge/ip-policies/` ],
+    [ fromIncludes(`/docs/platform/botusers/`), `/docs/user-management/#bot-users` ],
+    [ fromIncludes(`/docs/platform/dashboard/`), `/docs/user-management/#sso` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/webhook/`), `/docs/cloud-edge/modules/webhook-verification/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/amazon/`), `/docs/integrations/amazon/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/facebook/`), `/docs/integrations/facebook/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/github/`), `/docs/integrations/github/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/gitlab/`), `/docs/integrations/gitlab/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/google/`), `/docs/integrations/google/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/linkedin/`), `/docs/integrations/linkedin/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/microsoft/`), `/docs/integrations/microsoft/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/modules/oauth/twitch/`), `/docs/integrations/twitch/oauth/` ],
+    [ fromIncludes(`/docs/cloud-edge/http-header-templates/`), `/docs/cloud-edge/modules/request-headers/#variables` ],
+    [ fromIncludes(`/docs/integrations/awscloudwatch`), `/docs/integrations/amazon-cloudwatch/` ],
+    [ fromIncludes(`/docs/integrations/awsfirehose`), `/docs/integrations/amazon-firehose/` ],
+    [ fromIncludes(`/docs/integrations/awskinesis`), `/docs/integrations/amazon-kinesis/` ],
+    [ fromIncludes(`/docs/secure-tunnels/tunnels/tcp-tunnels`), `/docs/tcp/` ],
+    [ fromIncludes(`/docs/secure-tunnels/tunnels/ssh-reverse-tunnel-agent`), `/docs/agent/ssh-reverse-tunnel-agent` ],
+
+    // /docs/events/* -> /docs/obs/*
+    [ fromIncludes(`/docs/events/`), toReplace(`/docs/obs/`) ],
+
+    // redirects for ngrok agent, redirect cli first
+    [ fromIncludes(`/docs/secure-tunnels/ngrok-agent/reference/ngrok`), `/docs/agent/cli` ],
+    [ fromIncludes(`/docs/secure-tunnels/ngrok-agent/reference/`), toReplace(`/docs/agent/`) ],
+]
+
+// get current location from window
+const currentPath = window.location.pathname
+
+// setup empty variable to be filled when a match is found
+let newPath = null
+
+// iterate over each redirect, when a match is found, update newPath
+// we do this until the list is finished letting us stack redirects:
+// redirect A (2018) -> redirect B (2020) -> redirect C (current year)
+for (const redirect of redirects) {
+    let fromFn = redirect[0]
+    let toFn = redirect[1]
+
+    // Sugar for exact matching
+    if (typeof fromFn === 'string') {
+        fromFn = fromExact(fromFn)
+    }
+
+    // Sugar for exact replacement
+    if (typeof toFn === 'string') {
+        toFn = toExact(toFn)
+    }
+
+    const [from, fromResult] = fromFn(path)
+    if (fromResult) {
+        newPath = toFn(path, from)
+    }
 }
 
-// Just for exact match
-if (path.includes("/docs/ngrok-link")) goto(`/docs/cloud-edge`)
-if (path.includes("/docs/api/api-clients")) goto(`/docs/api/#client-libraries`)
-if (path.includes("/docs/api/client-libraries")) goto(`/docs/api/#client-libraries`)
-if (path.includes("/docs/api/terraform")) goto(`/docs/api/#terraform-provider`)
-if (path.includes("/docs/platform/api")) goto(`/docs/api/`)
-if (path.includes("/docs/platform/events")) goto(`/docs/events/`)
-if (path.includes("/docs/events/filtering")) goto(`/docs/events/#filters`)
-if (path.includes("/docs/http-header-templates/")) goto(`/docs/cloud-edge/http-header-templates/`)
-if (path.includes("/docs/platform/pops/")) goto(`/docs/cloud-edge/pops/`)
-if (path.includes("/docs/best-practices/security-dev-productivity/")) goto(`/docs/guides/security-dev-productivity/`)
-if (path.includes("/docs/platform/ip-policies/")) goto(`/docs/cloud-edge/ip-policies/`)
-if (path.includes("/docs/platform/botusers/")) goto(`/docs/user-management/#bot-users`)
-if (path.includes("/docs/platform/dashboard/")) goto(`/docs/user-management/#sso`)
-if (path.includes("/docs/cloud-edge/modules/webhook/")) goto(`/docs/cloud-edge/modules/webhook-verification/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/amazon/")) goto(`/docs/integrations/amazon/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/facebook/")) goto(`/docs/integrations/facebook/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/github/")) goto(`/docs/integrations/github/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/gitlab/")) goto(`/docs/integrations/gitlab/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/google/")) goto(`/docs/integrations/google/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/linkedin/")) goto(`/docs/integrations/linkedin/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/microsoft/")) goto(`/docs/integrations/microsoft/oauth/`)
-if (path.includes("/docs/cloud-edge/modules/oauth/twitch/")) goto(`/docs/integrations/twitch/oauth/`)
-if (path.includes("/docs/cloud-edge/http-header-templates/")) goto(`/docs/cloud-edge/modules/request-headers/#variables`)
-if (path.includes("/docs/integrations/awscloudwatch")) goto(`/docs/integrations/amazon-cloudwatch/`)
-if (path.includes("/docs/integrations/awsfirehose")) goto(`/docs/integrations/amazon-firehose/`)
-if (path.includes("/docs/integrations/awskinesis")) goto(`/docs/integrations/amazon-kinesis/`)
-if (path.includes("/docs/secure-tunnels/tunnels/tcp-tunnels/")) goto(`/docs/tcp/`)
-if (path.includes("/docs/secure-tunnels/tunnels/ssh-reverse-tunnel-agent/")) goto(`/docs/agent/ssh-reverse-tunnel-agent`)
-if (path.includes("/docs/secure-tunnels/ngrok-agent/reference/config/")) goto(`/docs/agent/config/`)
+// redirect when the path has changed
+if (newPath) {
+    window.location.href = `${window.location.origin}${newPath}`
+}
